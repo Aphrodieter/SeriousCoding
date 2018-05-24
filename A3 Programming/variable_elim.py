6bb7430d794493cf0d033e89b8e29291a8ab386a
Clemens <clemens-beissel@gmx.de>
2018-05-24 11:15:28 +0200

    SPML Programming A3

---
+++ A3 Programming/variable_elim.py
@@ -0,0 +1,67 @@
+"""
+@Author: Joris van Vugt, Moira Berens
+
+Implementation of the variable elimination algorithm for AISPAML assignment 3
+
+"""
+
+class VariableElimination():
+
+    def __init__(self, network):
+        self.network = network
+        self.addition_steps =  0
+        self.multiplication_steps = 0
+        
+        
+    def getChildren(self,node):
+        children = []
+        for n in self.network.parents:
+            if (node in self.network.parents[n]):
+                children.append(n)
+        return children
+                
+    def hasChildren(self,node):
+        for n in self.network.parents:
+            if (node in self.network.parents[n]):
+                return True
+        return False
+    
+    def isBarren(self,query,observerd,node):
+        if (node in query or node in observerd):
+            return False
+        elif (self.hasChildren(node)):
+            for child in self.getChildren(node):
+                if not self.isBarren(query,observerd,child):
+                    return False
+        return True
+        
+    
+
+    def run(self, query, observed, elim_order):
+        """
+        Use the variable elimination algorithm to find out the probability
+        distribution of the query variable given the observed variables
+
+        Input:
+            query:      The query variable
+            observed:   A dictionary of the observed variables {variable: value}
+            elim_order: Either a list specifying the elimination ordering
+                        or a function that will determine an elimination ordering
+                        given the network during the run
+
+        Output: A variable holding the probability distribution
+                for the query variable
+
+        """
+        p_query = []
+        nodes_of_interest = []
+        
+        for node in self.network.nodes:
+            if self.isBarren(query,observed, node):
+                print node
+                #p_query.append(self.network.probabilities[node])
+        
+        #print 'works?',p_query
+        #print self.hasChildren(elim_order[2])
+        #print self.getChildren('Alarm')
+
