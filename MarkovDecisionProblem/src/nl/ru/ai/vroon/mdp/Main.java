7d2748c285f950db9b66b3ac0bc33576c3f6cfb8
Clemens Beißel <c.beissel@student.ru.nl>
2018-08-15 19:57:12 +0200

    progressing ...

--- MarkovDecisionProblem/src/nl/ru/ai/vroon/mdp/Main.java
+++ MarkovDecisionProblem/src/nl/ru/ai/vroon/mdp/Main.java
@@ -12,25 +12,30 @@ public class Main {
 	 * @param args, not used
 	 */
 	public static void main(String[] args) {
-		MarkovDecisionProblem mdp = new MarkovDecisionProblem();
-		mdp.setInitialState(0, 0);
-                //ValueIterationAlgorithm v = new ValueIterationAlgorithm();
-		for (int i = 0; i < 15; i++){
-			mdp.performAction(Action.UP);
-			mdp.performAction(Action.UP);
-			mdp.performAction(Action.RIGHT);
-			mdp.performAction(Action.RIGHT);
-			mdp.performAction(Action.RIGHT);
-			mdp.restart();
-		}
+            
+            
+            ValueIterationAlgorithm v = new ValueIterationAlgorithm();
+            v.valueIteration();
+//		MarkovDecisionProblem mdp = new MarkovDecisionProblem();
+//		mdp.setInitialState(0, 0);
+                
+                
+//		for (int i = 0; i < 15; i++){
+//			mdp.performAction(Action.UP);
+//			mdp.performAction(Action.UP);
+//			mdp.performAction(Action.RIGHT);
+//			mdp.performAction(Action.RIGHT);
+//			mdp.performAction(Action.RIGHT);
+//			mdp.restart();
+//		}
 		
-		MarkovDecisionProblem mdp2 = new MarkovDecisionProblem(10, 10);
-		mdp2.setField(5, 5, Field.REWARD);
-		for (int i = 0; i < 100; i++){
-			mdp2.performAction(Action.UP);
-			mdp2.performAction(Action.RIGHT);
-			mdp2.performAction(Action.DOWN);
-			mdp2.performAction(Action.LEFT);
-		}
+//		MarkovDecisionProblem mdp2 = new MarkovDecisionProblem(10, 10);
+//		mdp2.setField(5, 5, Field.REWARD);
+//		for (int i = 0; i < 100; i++){
+//			mdp2.performAction(Action.UP);
+//			mdp2.performAction(Action.RIGHT);
+//			mdp2.performAction(Action.DOWN);
+//			mdp2.performAction(Action.LEFT);
+//		}
 	}
 }
--- MarkovDecisionProblem/src/nl/ru/ai/vroon/mdp/ValueIterationAlgorithm.java
+++ MarkovDecisionProblem/src/nl/ru/ai/vroon/mdp/ValueIterationAlgorithm.java
@@ -5,6 +5,11 @@
  */
 package nl.ru.ai.vroon.mdp;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  *
  * @author christianlammers
@@ -18,18 +23,45 @@ public class ValueIterationAlgorithm {
     public ValueIterationAlgorithm() {
         mdp = new MarkovDecisionProblem();
         mdp.setInitialState(0, 0);
+        stateUtilities = new Double[mdp.getWidth()][mdp.getHeight()];
+        oldStateUtilities = new Double[mdp.getWidth()][mdp.getHeight()];
         initStateUtility(oldStateUtilities);
+        
     }
 
     public void valueIteration() {
         for (int x = 0; x < mdp.getWidth(); x++) {
             for (int y = 0; y < mdp.getHeight(); y++) {
-                stateUtilities[x][y] = 
+                //System.out.println(oldStateUtilities[x][y]);
+                System.out.println(getBestAction(x,y) + " " + x + " , " + y);
+                //stateUtilities[x][y] = 
                
             }
 
         }
 
+    }
+    
+    private Action getBestAction(int x, int y){
+        Map<Double,Action> utils = new HashMap<>();
+        int right = x + 1;
+        int left = x - 1;
+        int up = y + 1;
+        int down = y - 1;
+        if (right < mdp.getWidth())
+            utils.put(oldStateUtilities[right][y],Action.RIGHT);
+        if (left >= 0)
+            utils.put(oldStateUtilities[left][y], Action.LEFT);
+        if (up < mdp.getHeight())
+            utils.put(oldStateUtilities[x][up], Action.UP);
+        if (down >= 0)
+            utils.put(oldStateUtilities[x][down], Action.DOWN);
+        
+        Set<Double> values = utils.keySet();
+        values.remove(null);
+        Action bestAction = utils.get(Collections.max(values)); 
+        
+        return bestAction;
     }
 
     /**
@@ -39,9 +71,8 @@ public class ValueIterationAlgorithm {
      * @param stateValues
      */
     private void initStateUtility(Double[][] oldStateUtilities) {
-        stateUtilities = new Double[mdp.getWidth()][mdp.getHeight()];
-        for (int i = 0; i < oldStateUtilities.length; i++) {
-            for (int j = 0; j < oldStateUtilities[i].length; j++) {
+        for (int i = 0; i < mdp.getWidth(); i++) {
+            for (int j = 0; j < mdp.getHeight(); j++) {
                 if (mdp.getField(i, j) == Field.EMPTY) {
                     oldStateUtilities[i][j] = 0.0;
                 } else if (mdp.getField(i, j) == Field.NEGREWARD) {
